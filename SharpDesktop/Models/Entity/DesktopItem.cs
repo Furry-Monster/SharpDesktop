3d796cf10a038a5f7704315b39d780b874db7fa8
FurryMonster <2876231904@qq.com>
2024-11-09 09:40:02 +0800

    backup and checkout

---
+++ SharpDesktop/Models/Entity/DesktopItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ReactiveUI;
+
+namespace SharpDesktop.Models.Entity;
+
+public class DesktopItem(string name, string? iconPath = null, string[]? files = null)
+{
+    public Guid Id { get; init; }
+
+    public string Name { get; set; } = name;
+
+    public string? IconPath { get; set; } = iconPath;
+
+    public string[]? Files { get; set; } = files;
+}
---
+++ SharpDesktop/Models/Entity/FileItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SharpDesktop.Models.Entity;
+
+public class FileItem(string name, string icon, string path)
+{
+    public Guid Id { get; init; }
+
+    public string Name { get; set; } = name;
+
+    public string Icon { get; set; } = icon;
+
+    public string Path { get; set; } = path;
+}
