e2333da3d20ff2d30ab2415d217bdf8ab8637dde
FurryMonster <2876231904@qq.com>
2024-11-13 15:43:40 +0800

    修缮了启动器图标加载逻辑，现在不硬性要求设定Background了

--- SharpDesktop/Util/Converter/PathToIcon.cs
+++ SharpDesktop/Util/Converter/PathToIcon.cs
@@ -1,15 +1,36 @@
 ﻿using System;
+using System.Drawing;
 using System.Globalization;
-using Avalonia.Media;
-using Avalonia.Media.Imaging;
+using System.IO;
+using Bitmap = Avalonia.Media.Imaging.Bitmap;
+#pragma warning disable CA1416
 
 namespace SharpDesktop.Util.Converter;
 
 public class PathToIcon : ConverterBase<string, Bitmap>
 {
+    /// <summary>
+    /// 采用stream方式读取图片，避免占用过多内存
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="parameter"></param>
+    /// <param name="culture"></param>
+    /// <returns></returns>
     public override Bitmap Convert(string? value, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        if (value == null)
+            return ImageLoader.LoadFromResource(new Uri("avares://SharpDesktop/Assets/icon.png"));
+
+        // 提取icon
+        using var icon = Icon.ExtractAssociatedIcon(value)!.ToBitmap();
+
+        // 保存icon到内存流
+        using var stream = new MemoryStream();
+        icon.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+        stream.Position = 0; // 重置流的位置
+
+        // 读取内存流
+        return new Bitmap(stream);
     }
 
     public override string ConvertBack(Bitmap? value, object? parameter, CultureInfo culture)
